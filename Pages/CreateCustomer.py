bacf7e0eea14728b4eb11c31e12b79173bb7636d
Jhoalejo07 <jhohan.arias@gmail.com>
2025-05-18 19:31:25 -0300

    Refactoring Classes and documenting_class CreateUsr

--- Pages/CreateCustomer.py
+++ Pages/CreateCustomer.py
@@ -2,14 +2,22 @@ from ProjectSeleniumPython.Pages.Functions import Functions
 
 
 class CreateUsr:
+    """
+    The CreateUsr class automates the user registration process on a web page.
 
-    '''
-     Composition is generally preferred over inheritance for building page objects, as it leads to more flexible and
-      maintainable code. For that reason, an instance of Functions is passed to SelectProduct as a dependency.
-    '''
+    Design Note:
+    Composition is used instead of inheritance to promote loose coupling and greater flexibility.
+    An instance of the `Functions` utility class is passed in to perform reusable browser actions.
+    """
 
     def __init__(self, functions: Functions, t: float):
+        """
+        Initializes the CreateUsr page object with locators and shared utilities.
 
+        Args:
+            functions (Functions): An instance of the Functions class used to perform web actions.
+            t (float): Time in seconds to wait during actions (e.g., delays for visibility).
+        """
         self.FIRST_NAME = "//input[contains(@name,'firstname')]"
         self.LAST_NAME = "//input[contains(@name,'lastname')]"
         self.EMAIL = "//input[@type='email'][contains(@id,'address')]"
@@ -21,24 +29,57 @@ class CreateUsr:
         self.t = t
 
     def EnterFirstName(self, p_firstName):
-        self.f.move_to_element("xpath", self.FIRST_NAME,  self.t)
-        self.f.input_text("xpath",self.FIRST_NAME, p_firstName,  self.t)
+        """
+        Enters the user's first name into the input field.
+
+        Args:
+            p_firstName (str): First name to input.
+        """
+        self.f.move_to_element("xpath", self.FIRST_NAME, self.t)
+        self.f.input_text("xpath", self.FIRST_NAME, p_firstName, self.t)
 
     def EnterLastName(self, p_lastName):
-        self.f.move_to_element("xpath",self.LAST_NAME,  self.t)
-        self.f.input_text("xpath",self.LAST_NAME, p_lastName,  self.t)
+        """
+        Enters the user's last name into the input field.
+
+        Args:
+            p_lastName (str): Last name to input.
+        """
+        self.f.move_to_element("xpath", self.LAST_NAME, self.t)
+        self.f.input_text("xpath", self.LAST_NAME, p_lastName, self.t)
 
     def EnterEmail(self, p_email):
-        self.f.move_to_element("xpath",self.EMAIL,  self.t)
-        self.f.input_text("xpath",self.EMAIL, p_email,  self.t)
+        """
+        Enters the user's email address.
+
+        Args:
+            p_email (str): Email to input.
+        """
+        self.f.move_to_element("xpath", self.EMAIL, self.t)
+        self.f.input_text("xpath", self.EMAIL, p_email, self.t)
 
     def EnterPassw(self, p_password):
-        self.f.move_to_element("xpath",self.PASSWORD,  self.t)
-        self.f.input_text("xpath",self.PASSWORD, p_password,  self.t)
+        """
+        Enters the password into the input field.
+
+        Args:
+            p_password (str): Password to input.
+        """
+        self.f.move_to_element("xpath", self.PASSWORD, self.t)
+        self.f.input_text("xpath", self.PASSWORD, p_password, self.t)
 
     def ConfirmPassw(self, p_password):
-        self.f.move_to_element("xpath",self.CONFIRM_PASS,  self.t)
-        self.f.input_text("xpath",self.CONFIRM_PASS, p_password,  self.t)
+        """
+        Re-enters the password into the confirmation field.
+
+        Args:
+            p_password (str): Password to confirm.
+        """
+        self.f.move_to_element("xpath", self.CONFIRM_PASS, self.t)
+        self.f.input_text("xpath", self.CONFIRM_PASS, p_password, self.t)
 
     def PressButtonCreate(self):
-        self.f.move_on_element_and_click("xpath", self.CREATE_BUTTON,  self.t)
+        """
+        Clicks the "Create an Account" button to submit the form.
+        """
+        self.f.move_on_element_and_click("xpath", self.CREATE_BUTTON, self.t)
